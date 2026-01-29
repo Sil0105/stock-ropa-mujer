45a688c65071befb550befed8764465c3efd9b79
Lourdes Torres <luli2000cuenta2.0@gmail.com>
2026-01-29 00:35:25 -0300

    Movi el front fuera del back

--- backend/middwares/error-middleware.js
+++ backend/middwares/error-middleware.js
@@ -1,7 +1,12 @@
-const errorMiddleware = (err, req, res, next) => {
+function errorMiddleware(err, req, res, next) {
+  console.error(err.stack);
+
+  const statusCode = err.status || 500;
+
   res.status(statusCode).json({
+    error: true,
     message: err.message || "Error interno del servidor",
   });
-};
+}
 
 module.exports = errorMiddleware;

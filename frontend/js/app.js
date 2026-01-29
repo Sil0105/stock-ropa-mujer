8cccbfd09c808d1c3182f5464e3371c3b039388a
Lourdes Torres <luli2000cuenta2.0@gmail.com>
2026-01-29 01:56:57 -0300

    Cambie la manera de pedir el token

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -90,7 +90,7 @@ const eliminarPrenda = async (id) => {
   await fetch(`${API_URL}/${id}`, {
     method: "DELETE",
     headers:{
-      "Authorization": "Bearer" + token
+      "Authorization": localStorage.getItem("token");
     }
   });
 
